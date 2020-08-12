a394003b588b785cb46e32a656f1a5915da42c7a
DivyaKuttoo <68158218+DivyaKuttoo@users.noreply.github.com>
2020-08-12 14:09:43 -0500

    minor changes

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,11 +2,3 @@ function gotoScienceMain() {
     window.location="./html/science-main.html";
 }
 
-$("#show-book").click(function () {
-    alert('test');
-    $.get("./html/books/test2.html", function (data) {
-        $("#book-rev").html(data);
-        // after loading open modal
-        $("#book-modal").modal('show');
-    });
-});
