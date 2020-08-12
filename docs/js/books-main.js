497c3ace35725d4ebc1afd78bfb34b1924476844
DivyaKuttoo <68158218+DivyaKuttoo@users.noreply.github.com>
2020-08-12 13:56:38 -0500

    Added book review

--- docs/js/books-main.js
+++ docs/js/books-main.js
@@ -17,3 +17,33 @@ function onPrev() {
   bookElement.children()[newLen-1].remove();
 
 };
+/*
+$("#book-rev").on("click",function(e){
+  alert('test');
+});
+*/
+
+$(".book-rev").click(function () {
+  var book_id = this.id;
+  $.get("./books/" + book_id + "_rev.html", function (data) {
+    
+    $("#book-modal").html(data);
+      // after loading open modal
+      $("#exampleModalCenter").modal('show');
+  });
+  
+});
+
+
+/*
+$("#book-rev-butt").click(function () {
+  
+  $.get("../html/books/brief_hist_rev.html", function (data) {
+  
+    $("#book-modal").html(data);
+      // after loading open modal
+      $("#exampleModalCenter").modal('show');
+  });
+});
+*/
+
